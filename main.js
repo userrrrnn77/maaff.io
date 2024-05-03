06bc76399f9c5a9fc4683a0de4d2ee3467b9ab2a
userrrrnn77 <165572436+userrrrnn77@users.noreply.github.com>
2024-05-04 02:46:00 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var namaPengguna = prompt("Hai! Siapa namamu?");
+
+var gombalan = [
+    "Kamu tau tidak, kamu adalah pesan yang paling sering aku balas ❤",
+    "Aku ingin menghabiskan sisa hidupku untuk membuatmu tersenyum setiap hari 🤞",
+    "Kamu tidak capek? Soalnya kamu sudah berputar terus di pikiranku 😜",
+    "Aku ingin menjadi penyebab kamu meleleh 😘",
+    "Kamu tau, kuputar-putar jam tanganku, tapi yang kujadikan pengukur waktu adalah betapa seringnya aku teringat padamu.",
+    "Hehe, apakah kamu tau hatiku berdetak lebih cepat setiap kali kamu senyum 😍",
+    "Kamu adalah bukti bahwa Surga itu nyata, surga itu kamu bukan epep ya 😂"
+];
+
+var gombalanAcak = gombalan[Math.floor(Math.random() * gombalan.length)];
+
+alert("Hai " + namaPengguna + "! sayang Aku punya gombalan untukmu: \n\n" + gombalanAcak);
+
+alert('aku minta maaf ya sayang kalo aku sering kasar sama kamu, aku sering marah marah, sering buat kamu nangis juga. maaf ya kalo aku banyak kurangnya, aku sayang banget sama kamu ' + namaPengguna)
+
+alert('semoga kamu engga salting brutal ya ' + namaPengguna + ' 😁hahay!!!')
+
+alert('boleh laa kirimi pap ke wa aku 😆')
